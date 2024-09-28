524fc7cb977ae77979477d65cf2ad7f9ea75155d
Tan Dao <ttdao3@asu.edu>
2024-09-28 01:19:03 -0700

    first commit

---
+++ app/src/main/java/com/example/luckynumber/SecoundActivity.java
@@ -0,0 +1,42 @@
+package com.example.luckynumber;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import org.w3c.dom.Text;
+
+public class SecoundActivity extends AppCompatActivity {
+
+    TextView welcomeTxt, luckyNumberTxt;
+    Button share_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_secound);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        welcomeTxt = findViewById(R.id.textView2);
+        luckyNumberTxt = findViewById(R.id.lucky_number_txt);
+        share_btn = findViewById(R.id.share_btn);
+
+        // Receiving the data from Main Activity
+        Intent i = getIntent();
+        String userName = i.getStringExtra("name");
+
+
+    }
+}
